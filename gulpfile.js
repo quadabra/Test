09d9f561fb0f48f1a68e3fefe3c3bf2db0313257
quadabra <gr.maks@gmail.com>
2017-12-11 02:03:35 +0300

    form 0.3 npm build

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,18 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var uglify = require('gulp-uglifyjs');
+var del = require("del");
+var run = require("run-sequence");
+
+gulp.task('uglify', function() {
+  gulp.src('js/*.js')
+    .pipe(uglify())
+    .pipe(rename("*.min.js"))
+    .pipe(gulp.dest('js'))
+});
 
 gulp.task("style", function () {
   gulp.src("less/style.less")
@@ -15,7 +27,26 @@ gulp.task("style", function () {
       autoprefixer()
     ]))
     .pipe(gulp.dest("css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("css"))
     .pipe(server.stream());
+});
+
+gulp.task("copy", function () {
+  return gulp.src([
+    "fonts/**/*.{woff,woff2}",
+    "img/**",
+    "js/*min.js",
+    "css/*min.css"
+  ], {
+    base: "."
+  })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function () {
+  return del("build");
 });
 
 gulp.task("serve", ["style"], function () {
@@ -29,4 +60,8 @@ gulp.task("serve", ["style"], function () {
 
   gulp.watch("less/**/*.less", ["style"]);
   gulp.watch("*.html").on("change", server.reload);
+});
+
+gulp.task("build", function (done) {
+  run ("clean", "style", "uglify", "copy", done);
 });
