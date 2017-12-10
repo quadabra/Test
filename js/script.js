b78e0bdebb29cad3c4e6bc02d2576cd8c1a8c653
quadabra <gr.maks@gmail.com>
2017-12-10 17:08:49 +0300

    form 0.2 html validation

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 var menuButton = document.querySelector('.navigation__menu');
 var menuContent = document.querySelector('.navigation__list');
+var paymentForm = document.querySelector('.payment-form');
+var inputNumbers = paymentForm.querySelectorAll('.payment-form__card-number');
 
 (function () {
   menuContent.classList.add('hidden');
