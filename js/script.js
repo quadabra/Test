1d4271160b9df1d641e4968f48a0ad7fe87cdb6b
quadabra <gr.maks@gmail.com>
2017-12-10 23:58:47 +0300

    form 0.2 html validation

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
+'use strict';
+
 var menuButton = document.querySelector('.navigation__menu');
 var menuContent = document.querySelector('.navigation__list');
 var paymentForm = document.querySelector('.payment-form');
 var inputNumbers = paymentForm.querySelectorAll('.payment-form__card-number');
+var inputCvc = paymentForm.querySelector('.payment-form__cvc-number');
 
 (function () {
   menuContent.classList.add('hidden');
@@ -10,3 +13,19 @@ var inputNumbers = paymentForm.querySelectorAll('.payment-form__card-number');
     menuButton.classList.toggle('sandwich-button_active')
   });
 })();
+
+var validateNumeric = function (evt) {
+  if (isNaN(evt.target.value)) {
+    evt.target.setCustomValidity('Введите цифры');
+    evt.target.style = 'outline: 1px solid red';
+  } else {
+    evt.target.setCustomValidity('');
+    evt.target.style = 'outline: none';
+  }
+};
+
+for (var i = 0; i < inputNumbers.length; i++) {
+  inputNumbers[i].addEventListener('invalid', validateNumeric);
+}
+
+inputCvc.addEventListener('invalid', validateNumeric);
